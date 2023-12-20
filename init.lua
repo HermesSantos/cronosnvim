a196c1665b96ffda9bd9166c1fa61d3999df337f
Hermes Santos <hermessantos995@gmail.com>
2023-12-20 02:14:47 -0300

    auto format and nvim-tree

--- init.lua
+++ init.lua
@@ -1,24 +1,30 @@
 -- Lazy initial
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 -- Lazy pluggins
 require("lazy").setup({
 	"craftzdog/solarized-osaka.nvim",
-    "williamboman/mason.nvim",
-    "williamboman/mason-lspconfig.nvim",
-    "neovim/nvim-lspconfig",
-    "WhoIsSethDaniel/mason-tool-installer.nvim"
+	"williamboman/mason.nvim",
+	"williamboman/mason-lspconfig.nvim",
+	"neovim/nvim-lspconfig",
+	"WhoIsSethDaniel/mason-tool-installer.nvim",
+	{
+		"stevearc/conform.nvim",
+		opts = {},
+	},
+	"nvim-tree/nvim-tree.lua",
+	"nvim-tree/nvim-web-devicons",
 })
 -- Setting up the colorscheme
 vim.cmd("colorscheme solarized-osaka")
@@ -28,3 +34,5 @@ require("keymaps")
 require("p-mason")
 require("p-lsp")
 require("lsp-lua")
+require("p-conform")
+require("p-tree")
